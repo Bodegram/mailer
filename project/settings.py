389cae09f9560de37502f7deb46cd633a22a5ab4
Bodegram <84967903+Bodegram@users.noreply.github.com>
2022-11-08 14:50:27 +0100

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -133,7 +133,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'mail.giitafrica.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'info@giitafrica.com'
-EMAIL_HOST_PASSWORD = 'jsRjlPAhvrCo'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 
